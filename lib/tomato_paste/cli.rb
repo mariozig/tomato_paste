91adbca3ec3893c145c2237ac3249a54bb6f31ec
mariozig <mariozig@gmail.com>
2013-04-23 14:24:49 -0700

    Cli class is mostly complete
    this brings an updated cli with tests that are close to being complete.  the major function of user input/output is tested… so that's good.

--- lib/tomato_paste/cli.rb
+++ lib/tomato_paste/cli.rb
@@ -1,4 +1,8 @@
+require 'tomato_paste'
+require 'tomato_paste/vine'
+require 'tomato_paste/pomodoro'
 require 'term/ansicolor'
+
 include Term::ANSIColor
 
 class String
@@ -7,14 +11,21 @@ end
 
 module TomatoPaste
   class Cli
-    def self.run
-      puts self.ascii_banner
-      vine = Vine.new
+    def initialize(input = $stdin, output = $stdout)
+      @input = input
+      @output = output
+      @vine = TomatoPaste::Vine.new
+    end
+
+    def run
+      @output.print ascii_banner
+      task_loop
+      @output.print "\n\nBye!"
     end
 
     # Prints the colored "logo" of sorts
     # "jgs" is the author of this awesome ascii tomato
-    def self.ascii_banner
+    def ascii_banner
       banner = "                     |  \n"
       banner += "         __\\W/__     ".green.bold + "|   _____                     _          ____           _       \n"
       banner += "       .'.-'|'-.'.   ".red.bold + "|  |_   _|__  _ __ ___   __ _| |_ ___   |  _ \\ __ _ ___| |_ ___ \n"
@@ -23,8 +34,35 @@ module TomatoPaste
       banner += "       \\         /   ".red.bold + "|    |_|\\___/|_| |_| |_|\\__,_|\\__\\___/  |_|   \\__,_|___/\\__\\___|\n"
       banner += "   jgs".yellow.bold + "  '-.___.-'    ".red.bold + "|  \n"
       banner += "                     |   \n"
+      banner += "                  ...it's Pomodoro time.\n"
 
       banner
+    end
+
+    def task_loop
+      begin
+        @output.print "\n\n"
+        @output.print "Task Description: "
+        task_description = @input.gets.chomp
+
+        @vine.add(TomatoPaste::Pomodoro.new(task_description))
+        @output.puts "Starting Pomodoro ##{@vine.pomodori.count}"
+        @vine.current_pomodoro.work_timer.start
+
+        if @vine.big_break_time?
+          @output.puts "It's time for a big break. Come back in 20 minutes."
+          # big_break = Timer.new(1200) # 20 minutes
+          big_break = Timer.new(5) # 20 minutes
+          big_break.start
+        else
+          @output.puts "Pomodoro ##{@vine.pomodori.count} complete! Break starts now."
+          @vine.current_pomodoro.break_timer.start
+        end
+
+        @output.puts "Break done!\n"
+
+        @output.print "Do another Pomodoro? (Y/N): "
+      end while @input.gets.upcase.chomp != "N"
     end
 
   end
--- spec/tomato_paste/cli_spec.rb
+++ spec/tomato_paste/cli_spec.rb
@@ -2,12 +2,32 @@ require 'spec_helper'
 
 describe TomatoPaste::Cli do
 
-  describe "#run" do
-    it "shows logo/banner by calling #ascii_banner" do
-      described_class.should_receive(:ascii_banner)
-      described_class.run
+  describe "#ascii_banner" do
+    subject(:cli) { described_class.new }
+
+    its(:ascii_banner) { should be_a(String) }
+  end
+
+  describe "task_loop" do
+    it "successfully performs a single pomodoro" do
+      input_one_task = StringIO.new("pomodoro_description\nN\n")
+      output = StringIO.new
+
+      described_class.new(input_one_task, output).task_loop
+
+      output.string.should include("Starting Pomodoro #1", "Pomodoro #1 complete!")
+      output.string.should_not include("#2")
     end
 
+    it "successfully performs multiple pomodori" do
+      input_two_tasks = StringIO.new("pomodoro1\ny\npomodoro2\nN\n")
+      output = StringIO.new
+
+      described_class.new(input_two_tasks, output).task_loop
+
+      output.string.should include("Starting Pomodoro #1", "Pomodoro #1 complete!")
+      output.string.should include("Starting Pomodoro #2", "Pomodoro #2 complete!")
+    end
   end
 
 end
