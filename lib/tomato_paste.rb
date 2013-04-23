ad5de998968b8e43d0bd18538bb9df3ef152740d
mariozig <mariozig@gmail.com>
2013-04-23 14:23:09 -0700

    require everything

--- lib/tomato_paste.rb
+++ lib/tomato_paste.rb
@@ -1,4 +1,8 @@
 require "tomato_paste/version"
+require "tomato_paste/cli"
+require "tomato_paste/timer"
+require "tomato_paste/vine"
+require "tomato_paste/pomodoro"
 
 module TomatoPaste
   # Your code goes here...
