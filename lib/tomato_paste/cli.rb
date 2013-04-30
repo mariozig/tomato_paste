2f3ef5e118667e1ec830eaa77da33f58b7075764
mariozig <mariozig@gmail.com>
2013-04-29 17:14:40 -0700

    Make ascii art fit nicely in an 80 col terminal

--- lib/tomato_paste/cli.rb
+++ lib/tomato_paste/cli.rb
@@ -23,15 +23,15 @@ module TomatoPaste
     # Prints the colored "logo" of sorts
     # "jgs" is the author of this awesome ascii tomato
     def ascii_banner
-      banner = "                  |  \n"
-      banner += "      __\\W/__     ".green.bold + "|   _____                     _          ____           _       \n"
-      banner += "    .'.-'|'-.'.   ".red.bold + "|  |_   _|__  _ __ ___   __ _| |_ ___   |  _ \\ __ _ ___| |_ ___ \n"
-      banner += "   /           \\  ".red.bold + "|    | |/ _ \\| '_ ` _ \\ / _` | __/ _ \\  | |_) / _` / __| __/ _ \\\n"
-      banner += "   |           |  ".red.bold + "|    | | (_) | | | | | | (_| | || (_) | |  __/ (_| \\__ \\ ||  __/\n"
-      banner += "    \\         /   ".red.bold + "|    |_|\\___/|_| |_| |_|\\__,_|\\__\\___/  |_|   \\__,_|___/\\__\\___|\n"
-      banner += "jgs".yellow.bold + "  '-.___.-'    ".red.bold + "|  \n"
-      banner += "                  |   \n"
-      banner += "                                                               ...a Pomodoro timer\n"
+      banner = "               |  \n"
+      banner += "   __\\W/__     ".green.bold + "|   _____                     _          ____           _       \n"
+      banner += " .'.-'|'-.'.   ".red.bold + "|  |_   _|__  _ __ ___   __ _| |_ ___   |  _ \\ __ _ ___| |_ ___ \n"
+      banner += "/           \\  ".red.bold + "|    | |/ _ \\| '_ ` _ \\ / _` | __/ _ \\  | |_) / _` / __| __/ _ \\\n"
+      banner += "|           |  ".red.bold + "|    | | (_) | | | | | | (_| | || (_) | |  __/ (_| \\__ \\ ||  __/\n"
+      banner += " \\         /   ".red.bold + "|    |_|\\___/|_| |_| |_|\\__,_|\\__\\___/  |_|   \\__,_|___/\\__\\___|\n"
+      banner += "  '-.___.-'    ".red.bold + "|  \n"
+      banner += "jgs".yellow.bold + "            |   \n"
+      banner += "                                                           ...a Pomodoro timer\n"
 
       banner
     end
